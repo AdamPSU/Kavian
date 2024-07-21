3a1e0fb2f89b875d13d414d1979d49fd6b189b5f
Adam Torres Encarnacion <admr0805@gmail.com>
2024-07-21 11:03:03 -0400

    Add basic skeleton for Summary() class

---
+++ kavian/tables/summaries.py
@@ -0,0 +1,11 @@
+class Summary:
+    def __init__(self, header: str, features: list, verbose=True):
+        self.header = header
+        self.verbose = verbose
+        self.features = features
+
+    def get_features(self):
+        pass
+
+    def feature_frame(self):
+        pass
